2b41606efe5dd541b5bcb7e21f428f869d3b7476
eleen1102-cmd <eleen1102@gmail.com>
2026-06-03 17:29:34 -0400

    coding score and resetting scene when loss

--- Assets/Scripts/BirdMovement.cs
+++ Assets/Scripts/BirdMovement.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
-//NEED TO ADD: 
-//animations 
-//stop bird from tilting over (or control the tilting since it is actually in flappy bird)
+using TMPro; //needed for text mesh pro library
+using UnityEngine.SceneManagement; //to reset the scene 
 public class BirdMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
@@ -10,6 +9,11 @@ public class BirdMovement : MonoBehaviour
     private PolygonCollider2D polygonCollider;
     private float horizontalInput;
     private Animator anim;
+
+    public TextMeshProUGUI scoreText;
+
+    private int score;
+
     
     
     //awake is called everytime tbe script is loaded
@@ -19,6 +23,7 @@ public class BirdMovement : MonoBehaviour
         body = GetComponent<Rigidbody2D>(); //finds rigibody2D and will store it in the body
         polygonCollider = GetComponent<PolygonCollider2D>();
         anim = GetComponent<Animator>();
+
     }
 
    
@@ -28,11 +33,26 @@ public class BirdMovement : MonoBehaviour
 
         if (Input.GetKey(KeyCode.Space))
             Jump();
-
+  
 
 
     }
 
+    //score
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("ScoreTrigger"))
+        {
+            score++;
+            scoreText.text = score.ToString();
+        }
+    }
+
+    //resetting the game after loss
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
     private void Jump()
     {
         body.linearVelocity = new Vector2(0, jumpPower);
