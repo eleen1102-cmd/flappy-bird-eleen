d0f0a2e65232fa1e5d53b410066db471f6c59913
eleen1102-cmd <eleen1102@gmail.com>
2026-06-05 15:40:00 -0400

    debugging text
    issue with assigning tag, also adjusted the size of the text

--- Assets/Scripts/BirdMovement.cs
+++ Assets/Scripts/BirdMovement.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using TMPro; //needed for text mesh pro library
 using UnityEngine.SceneManagement; //to reset the scene 
+//using System.Runtime.CompilerServices; 
 
 //NEED TO FIX:
 //make pipes spawn faster
@@ -8,12 +9,14 @@ using UnityEngine.SceneManagement; //to reset the scene
 //make collision with pipes work (might have to do with rb)
 public class BirdMovement : MonoBehaviour
 {
-    [SerializeField] private float speed;
-    [SerializeField] private float jumpPower;
-    private Rigidbody2D body;
+    [SerializeField] public float speed;
+    [SerializeField] public float jumpPower;
+    public Rigidbody2D body;
     private PolygonCollider2D polygonCollider;
+   // private BoxCollider2D boxCollider;
     private float horizontalInput;
     private Animator anim;
+    
 
     public TextMeshProUGUI scoreText;
 
@@ -27,12 +30,13 @@ public class BirdMovement : MonoBehaviour
         //grab references for rigidbody and animator 
         body = GetComponent<Rigidbody2D>(); //finds rigibody2D and will store it in the body
         polygonCollider = GetComponent<PolygonCollider2D>();
+      // boxCollider = GetComponent<BoxCollider2D>();
         anim = GetComponent<Animator>();
 
     }
 
    
-    private void Update()
+    private void FixedUpdate()
     {
         horizontalInput = Input.GetAxis("Horizontal"); //idk if i actually need this rn
 
